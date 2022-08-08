cc59a3aae93f56b2d237bfe822b274e8243f8a06
kessio <64524758+kessio@users.noreply.github.com>
2022-08-08 15:25:40 +0300

    Added class, values and render modules

---
+++ modules/getLocalStorage.js
@@ -0,0 +1,9 @@
+export function getLocalStorageBooks() {
+    let bookks;
+    if (localStorage.getItem('books') === null) {
+      bookks = [];
+    } else {
+      bookks = JSON.parse(localStorage.getItem('books'));
+    }
+    return bookks;
+  }
